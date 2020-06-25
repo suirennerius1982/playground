a79424a7557bb9bb93097245e705f2ffd8d81277
nerius.perez <ntoirac@proveedor.teco.com.ar>
2020-06-25 20:55:33 -0300

    Example and comparative promise only and promise chaining

--- 8-promises.js
+++ 8-promises.js
@@ -1,4 +1,39 @@
-const { resolve } = require("path");
+const { setupMaster } = require("cluster")
+
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+//Using promise chaining
+
+add(1, 2).then((result) => {
+    console.log(result)
+    return add(result, 5)
+}).then((result) => {
+    console.log(result)
+}).catch((error) => {
+    console.log(error)
+})
+
+//Example without promise chaining:
+
+/* add(1, 2).then((result) => {
+    console.log(result)
+
+    add(result, 5).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
+    })
+}).catch((error) => {
+    console.log(error)
+}) */
+
+/* const { resolve } = require("path");
 const { rejects } = require("assert");
 
 const doWorkPromise = new Promise((resolve, reject) => {
@@ -12,7 +47,7 @@ doWorkPromise.then((result) => {
     console.log('Success!', result)
 }).catch((error) => {
     console.log('Error!!!', error)
-})
+}) */
 
 //
 //
